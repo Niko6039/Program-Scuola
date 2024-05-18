99248de250602a70ebbb7ee46c4386f11dc71c99
Home-pc <nicolo.tauro@iisstecnicomonopoli.it>
2024-05-18 02:09:04 +0200

    modofica

--- src/Program2/Playlist.java
+++ src/Program2/Playlist.java
@@ -8,21 +8,35 @@ import java.awt.event.ActionListener;
 import java.util.Queue;
 
 public class Playlist extends JDialog {
-    JButton Aventi;
-    JLabel testo;
+    JButton Aventi,Cancel;
+    JLabel testo, logoLabel;
+    JPanel imagePanel;
+    ImageIcon image;
     public Playlist(Queue coda) {
         setTitle("Playlist");
-        testo = new JLabel();
+        setModal(true);
         setLocationRelativeTo(null);
-        setSize(300,200);
+        setSize(350,200);
+        setBackground(new Color(16, 188, 76));
+
+        imagePanel = new JPanel();
+        imagePanel.setBackground(new Color(16, 188, 76));
+        logoLabel = new JLabel();
+        image = new ImageIcon(Main.path + "img2.png");
+        logoLabel.setIcon(image);
+        add(logoLabel, BorderLayout.WEST);
+
+        testo = new JLabel();
         if(coda.isEmpty()!=true){
             testo.setText(coda.poll().toString());
         }
 
-
-        JPanel panel = new JPanel();
         Aventi = new JButton("Aventi");
+        JPanel panel = new JPanel();
         panel.add(Aventi);
+        panel.setBackground(new Color(16, 188, 76));
+        add(panel, BorderLayout.SOUTH);
+
         Aventi.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (coda.isEmpty()!=true){
@@ -30,11 +44,12 @@ public class Playlist extends JDialog {
                     testo.setText(coda.poll().toString());
                 }else{
                     testo.setText("");
+                    add(logoLabel, BorderLayout.CENTER);
+
                 }
             }
         });
         add(testo, BorderLayout.CENTER);
-        add(panel, BorderLayout.SOUTH);
         setVisible(true);
     }
 }
